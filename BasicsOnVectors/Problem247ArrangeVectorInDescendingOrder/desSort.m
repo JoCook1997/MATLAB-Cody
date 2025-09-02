d4170e2a4ebf78f6f18bb1820dacad54f7050a34
JoCook1997 <62695991+JoCook1997@users.noreply.github.com>
2025-09-02 15:32:01 +0100

    Sort a row or column vector in descending order

---
+++ BasicsOnVectors/Problem247ArrangeVectorInDescendingOrder/desSort.m
@@ -0,0 +1,21 @@
+function y = desSort(x)
+%desSort(x) will sort an array x into descending order
+
+% check it's not a matrix first
+if all(size(x)>1)
+    error("Must be a horizontal or vertical array to perform sum operation")
+end
+
+% determine if its a row or column vector
+if isrow(x)
+    dimension=2;
+end
+
+if iscolumn(x)
+    dimension=1;
+end
+
+% perform sort
+y=sort(x,dimension,"descend");
+
+end
